bb77bd5ef6a1b1a4f2d119ad07d422f8345e516b
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-11-08 17:10:19 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/pinia">Pinia</RouterLink>
       <router-view></router-view>
     </div>
   </div>
